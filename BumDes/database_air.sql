77fac88aac8b3be8e78d9e2296c13289b7978caf
black.paradekid@gmail.com <black.paradekid@gmail.com@20a926d0-a0eb-7dfd-84ed-55b6f22d195c>
2015-02-16 17:57:08 +0000

    Kelas

---
+++ BumDes/database_air.sql
@@ -0,0 +1,79 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.05 
+MySQL - 5.5.16 : Database - airbersih
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`airbersih` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `airbersih`;
+
+/*Table structure for table `barang` */
+
+DROP TABLE IF EXISTS `barang`;
+
+CREATE TABLE `barang` (
+  `idbarang` varchar(10) NOT NULL,
+  `namabarang` varchar(100) NOT NULL,
+  `stokbarang` int(100) NOT NULL,
+  PRIMARY KEY (`idbarang`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `barang` */
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`username`,`password`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `login` */
+
+insert  into `login`(`username`,`password`) values ('AIR','AIR');
+
+/*Table structure for table `pegawai` */
+
+DROP TABLE IF EXISTS `pegawai`;
+
+CREATE TABLE `pegawai` (
+  `idpegawai` varchar(10) NOT NULL,
+  `namapegawai` varchar(100) DEFAULT NULL,
+  `jabatan` varchar(100) DEFAULT NULL,
+  `alamat` varchar(200) DEFAULT NULL,
+  `telp` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idpegawai`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `pegawai` */
+
+/*Table structure for table `transaksi` */
+
+DROP TABLE IF EXISTS `transaksi`;
+
+CREATE TABLE `transaksi` (
+  `notransaksi` varchar(10) NOT NULL,
+  `idbarang` varchar(10) NOT NULL,
+  `idpegawai` varchar(10) NOT NULL,
+  `tanggaltransaksi` date DEFAULT NULL,
+  `jenistransaksi` varchar(10) DEFAULT NULL,
+  `jumlah` int(20) DEFAULT NULL,
+  `hargasatuan` int(20) DEFAULT NULL,
+  `total` int(40) DEFAULT NULL,
+  PRIMARY KEY (`notransaksi`,`idbarang`,`idpegawai`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `transaksi` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
