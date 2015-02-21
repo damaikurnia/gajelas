b1c11d0929649db13abb2b5da810c1a4044245b2
black.paradekid@gmail.com <black.paradekid@gmail.com@20a926d0-a0eb-7dfd-84ed-55b6f22d195c>
2015-02-21 13:42:20 +0000

    database

--- trunk/BumDes/database_air.sql
+++ trunk/BumDes/database_air.sql
@@ -1,51 +1,77 @@
-/*
-SQLyog Enterprise - MySQL GUI v8.05 
-MySQL - 5.5.16 : Database - airbersih
-*********************************************************************
-*/
+CREATE DATABASE  IF NOT EXISTS `airbersih` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `airbersih`;
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: airbersih
+-- ------------------------------------------------------
+-- Server version	5.5.41-0ubuntu0.14.04.1
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`airbersih` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `airbersih`;
-
-/*Table structure for table `barang` */
+--
+-- Table structure for table `barang`
+--
 
 DROP TABLE IF EXISTS `barang`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `barang` (
   `idbarang` varchar(10) NOT NULL,
   `namabarang` varchar(100) NOT NULL,
   `stokbarang` int(100) NOT NULL,
   PRIMARY KEY (`idbarang`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `barang` */
+--
+-- Dumping data for table `barang`
+--
 
-/*Table structure for table `login` */
+LOCK TABLES `barang` WRITE;
+/*!40000 ALTER TABLE `barang` DISABLE KEYS */;
+/*!40000 ALTER TABLE `barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
 
 DROP TABLE IF EXISTS `login`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `login` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
   PRIMARY KEY (`username`,`password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `login` */
+--
+-- Dumping data for table `login`
+--
 
-insert  into `login`(`username`,`password`) values ('AIR','AIR');
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES ('AIR','AIR');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `pegawai` */
+--
+-- Table structure for table `pegawai`
+--
 
 DROP TABLE IF EXISTS `pegawai`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pegawai` (
   `idpegawai` varchar(10) NOT NULL,
   `namapegawai` varchar(100) DEFAULT NULL,
@@ -54,13 +80,50 @@ CREATE TABLE `pegawai` (
   `telp` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`idpegawai`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `pegawai` */
+--
+-- Dumping data for table `pegawai`
+--
 
-/*Table structure for table `transaksi` */
+LOCK TABLES `pegawai` WRITE;
+/*!40000 ALTER TABLE `pegawai` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pegawai` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profil`
+--
+
+DROP TABLE IF EXISTS `profil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profil` (
+  `idprofil` int(11) NOT NULL,
+  `namadesa` varchar(100) NOT NULL,
+  `alamatdesa` varchar(200) NOT NULL,
+  `notelp` varchar(45) NOT NULL,
+  PRIMARY KEY (`idprofil`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profil`
+--
+
+LOCK TABLES `profil` WRITE;
+/*!40000 ALTER TABLE `profil` DISABLE KEYS */;
+INSERT INTO `profil` VALUES (0,'DESA SUKAMUNDUR','JL PAINGAN ','081212321211');
+/*!40000 ALTER TABLE `profil` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaksi`
+--
 
 DROP TABLE IF EXISTS `transaksi`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transaksi` (
   `notransaksi` varchar(10) NOT NULL,
   `idbarang` varchar(10) NOT NULL,
@@ -72,8 +135,24 @@ CREATE TABLE `transaksi` (
   `total` int(40) DEFAULT NULL,
   PRIMARY KEY (`notransaksi`,`idbarang`,`idpegawai`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `transaksi` */
+--
+-- Dumping data for table `transaksi`
+--
+
+LOCK TABLES `transaksi` WRITE;
+/*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-02-21 20:41:38
