ad3ceb3014416a60f4fadca1af1bd89cea3b52c6
black.paradekid@gmail.com <black.paradekid@gmail.com@20a926d0-a0eb-7dfd-84ed-55b6f22d195c>
2015-02-23 15:13:23 +0000

    TRANSAKSI : PREPARE PREPARE

---
+++ trunk/BumDes/src/Kelas/Tanggal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kelas;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author damaikurnia
+ */
+public class Tanggal {
+    public static String getTanggal() { 
+//        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        DateFormat dateFormat = new SimpleDateFormat("EEEEEE,d MMM yyyy");
+        Date date = new Date(); 
+        return dateFormat.format(date); 
+    } 
+     
+//    public static void main(String Args[]){ 
+//        Tanggal tgl = new Tanggal(); 
+//        System.out.println("Tanggal dan waktu sekarang : "+tgl.getTanggal()); 
+//    } 
+} 
+
