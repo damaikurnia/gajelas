e3e6548fca091c9d749cade27d7648b566759098
black.paradekid@gmail.com <black.paradekid@gmail.com@20a926d0-a0eb-7dfd-84ed-55b6f22d195c>
2015-03-08 05:13:16 +0000

    Tambah query untuk pemakaian air

---
+++ trunk/BumDes/src/Kelas/Pemakaian.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kelas;
+
+/**
+ *
+ * @author damaikurnia
+ */
+public class Pemakaian {
+    String notransaksi;
+    Anggota idanggota;
+    double airlunas;
+    double airterakhir;
+    double airdibayar;
+    String tanggal;
+    
+    /**
+     * @return the notransaksi
+     */
+    public String getNotransaksi() {
+        return notransaksi;
+    }
+
+    /**
+     * @param notransaksi the notransaksi to set
+     */
+    public void setNotransaksi(String notransaksi) {
+        this.notransaksi = notransaksi;
+    }
+
+    /**
+     * @return the idanggota
+     */
+    public Anggota getIdanggota() {
+        return idanggota;
+    }
+
+    /**
+     * @param idanggota the idanggota to set
+     */
+    public void setIdanggota(Anggota idanggota) {
+        this.idanggota = idanggota;
+    }
+
+    /**
+     * @return the airlunas
+     */
+    public double getAirlunas() {
+        return airlunas;
+    }
+
+    /**
+     * @param airlunas the airlunas to set
+     */
+    public void setAirlunas(double airlunas) {
+        this.airlunas = airlunas;
+    }
+
+    /**
+     * @return the airterakhir
+     */
+    public double getAirterakhir() {
+        return airterakhir;
+    }
+
+    /**
+     * @param airterakhir the airterakhir to set
+     */
+    public void setAirterakhir(double airterakhir) {
+        this.airterakhir = airterakhir;
+    }
+
+    /**
+     * @return the tanggal
+     */
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    /**
+     * @param tanggal the tanggal to set
+     */
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    /**
+     * @return the airdibayar
+     */
+    public double getAirdibayar() {
+        return airdibayar;
+    }
+
+    /**
+     * @param airdibayar the airdibayar to set
+     */
+    public void setAirdibayar(double airdibayar) {
+        this.airdibayar = airdibayar;
+    }
+}
